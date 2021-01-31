2db864d0ec4c553d770d64f0e9dfb027f696d578
ankitbatra2 <ankitbatra2202@gmail.com>
2021-01-30 22:50:46 -0500

    fixed api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import React from "react";
+import React, {Component, useState, useEffect} from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from './components/weather.component'
 import 'weather-icons/css/weather-icons.css'
 
-function App() {
+const key = 'b2788703be160b952a9a79994fb6485b'
+
+function Tweather() {
+  const[data, setData] = useState(null);
+  useEffect(() => {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b2788703be160b952a9a79994fb6485b')
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error)
+  }, []);
   return (
-    <div className="App">
-      <Weather/>
+    <div>
+      <h1>{JSON.stringify(data)}</h1>
     </div>
+  )
+}
+
+/*
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    //this.getWeather();
+    Tweather();
+  }
+
+getWeather = async() => {
+  const api_call = await fetch('api.openweathermap.org/data/2.5/weather?q=London,uk&appid='+key);
+  const response  = await api_call.json();
+  console.log(response)
+}
+
+render() {
+  return(
+    <div className="App">
+    <Weather/>
+  </div>
+  
   );
 }
+
+}
+*/
+
+function App() {
+  return (
+    <Tweather/>
+  );
+  }
 
 export default App;
 
--- src/components/weather.component.jsx
+++ src/components/weather.component.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 
 function Weather() {
   return (
-    <div className="container"> 
-    <h1>Weather App Inital</h1>
-    </div>
+   // <div className="background">
+      <div className="container"> 
+        <div className="card">
+          <h1>Weather App Inital</h1>
+          <h5 className="py-4">
+            <i className="wi wi-day-sunny display-10" ></i>
+          </h5>
+          <h1 className="py-2">25&deg;</h1>
+        </div>
+      </div>
+    //</div>
   );
 };
 
+
+
 export default Weather;
+
