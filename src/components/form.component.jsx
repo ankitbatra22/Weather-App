8637a1838300ed84fc9c6840f6ee07cc7d698460
ankitbatra2 <ankitbatra2202@gmail.com>
2021-02-09 14:55:20 -0500

    fixed input placeholder

--- src/components/form.component.jsx
+++ src/components/form.component.jsx
@@ -7,10 +7,20 @@ const Form = props =>{
     <div className = "container">
       <div className="row">
         <div className="col-md-3 offset-md-2">
-          <input type="text" className="form-control" name="city" autoComplete="off" placeholder="City"></input>
+          <input 
+            type="text" 
+            className="form-control" 
+            name="city" 
+            autoComplete="off" 
+            placeholder="City"></input>
         </div>
         <div className="col-md-3">
-          <input type="text" className="form-control" name="country" autoComplete="off" placeholder="Country"></input>
+          <input 
+            type="text" 
+            className="form-control" 
+            name="country" 
+            autoComplete="off" 
+            placeholder="Country"></input>
         </div>
         <div className="col-md-3 mt-md-0 text-md-left">
           <button className ="btn btn-info">Get Weather</button>
