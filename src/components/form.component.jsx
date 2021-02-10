f5df50903872a71a43f6b854a569706165095838
ankitbatra2 <ankitbatra2202@gmail.com>
2021-02-09 21:59:25 -0500

    updated

--- src/components/form.component.jsx
+++ src/components/form.component.jsx
@@ -4,7 +4,9 @@ import './form.css'
 const Form = props =>{
   return(
     <form onSubmit={props.loadweather}>
+    <h1 className="text-center pb-5 text-light"> Ankit's Weather App </h1>
     <div className = "container">
+      <div>{props.error ? error(): null}</div>
       <div className="row">
         <div className="col-md-3 offset-md-2">
           <input 
@@ -23,11 +25,20 @@ const Form = props =>{
             placeholder="Country"></input>
         </div>
         <div className="col-md-3 mt-md-0 text-md-left">
-          <button className ="btn btn-info">Get Weather</button>
+          <button className ="btn btn-warning">Get Weather</button>
         </div>
       </div>
     </div>
   </form>
   );
+}
+
+
+function error() {
+  return(
+    <div className = "alert alert-danger mx-5" role = "alert">
+      Please Enter the City and Country
+    </div>
+  )
 }
 export default Form;
--- src/components/weather.component.jsx
+++ src/components/weather.component.jsx
@@ -3,31 +3,33 @@ import { createPortal } from 'react-dom';
 
 function Weather(props) {
   return (
-      <div className="container"> 
+      <div className="container text-light text-center mx-auto p-3 mt-2"> 
         <div className="cards pt-4">
-          <h1>Ankit's Weather App</h1>
-          <h2>{props.city}, {props.country}</h2>
-          <h5 className="py-4">
-            <i className={props.icon}></i>
-          </h5>
-          <h1 className="py-2">{props.celsius}&deg;</h1>
+          <h2 className="text-light text-center">{props.city}</h2>
+          <h1 className="py-4 text-center text-light pt-3">
+            <i className={`wi ${props.icon} display-1`}></i>
+          </h1>
+          {props.celsius ? (<h1 className= "py-2 text-center text-light"> {props.celsius}&deg;</h1>): null}
+
           {showMinMax(props.min, props.max)}
-          <h2>{props.description}</h2>
+          <h2 className="py-3">
+          {props.description.charAt(0).toUpperCase() +
+            props.description.slice(1)}
+        </h2>
         </div>
       </div>
-    //</div>
   );
 };
-
-
 
 export default Weather;
 
 function showMinMax(min, max) {
+  if (min && max){
   return (
     <h3>
-      <span className="px-4">{min}&deg;</span>
-      <span className="px-4">{max}&deg;</span> 
+      <span className="px-4">Low: {min}&deg;</span>
+      <span className="px-4">High: {max}&deg;</span> 
     </h3>
-  )
+  );
+}
 }
